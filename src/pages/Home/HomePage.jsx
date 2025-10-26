c1260f94f430ae74f7fd11641ce7debb97479820
sayehxp <sayehxp@gmail.com>
2025-10-27 01:36:20 +0300

    feat: reorganize imports and update component structure in HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,17 +1,22 @@
-import HomeHero from '../../components/HomeHero/HomeHero.JSX'
+import Demo from '../../components/Demo/Demo'
+import HomeHero from '../../components/HomeHero/HomeHero'
 import LinkedinModal from '../../components/LinkedinModal/LinkedinModal'
 import ResumeForm from '../../components/ResumeForm/ResumeForm'
+import WhyChooseUs from '../../components/WhyChooseUs/WhyChooseUs'
 import './HomePage.css'
+import Music from './../../components/Music/Music';
 const HomePage = () => {
     return (
         <>
-         
+
 
             {/* <HomeHero /> */}
             <div className='row m-0  '>
                 <HomeHero />
+                {/* <Music /> */}
+                {/* <Demo /> */}
                 <ResumeForm />
-             
+                <WhyChooseUs />
 
             </div>
             <LinkedinModal />
